c0aaa91fe228a973ca4edb1b3cb0d580b0e5ac04
theonlydude <theonlydude@server.fake>
2021-09-05 15:02:05 +0200

    ship: add Ice Metal Uninstall & Lost World ships

--- custom_ships.py
+++ custom_ships.py
@@ -6,6 +6,8 @@ customShips = {
     'Red-M0nk3ySMShip5': {"name": "Red-M0nk3ySMShip5", "desc": "From MetConst", "author": "Red-M0nk3y"},
     'opposition_ship': {"name": "Opposition Ship", "desc": "From Opposition Hack", "author": "mccad00"},
     'ascent_ship': {"name": "Ascent Ship", "desc": "From Ascent Hack", "author": "Benox50"},
+    'ice_metal_ship': {"name": "Ice Metal Uninstall", "desc": "From Ice Metal Uninstall Hack", "author": "Crys"},
+    'lost_world_ship': {"name": "Lost World", "desc": "From Lost World Hack", "author": "TROM"},
     'mario_ship': {"name": "Mario Ship", "desc": "For Mario and Luigi sprites", "author": "mccad00"}
 }
 
@@ -17,5 +19,7 @@ customShipsOrder = [
     'Red-M0nk3ySMShip5',
     'opposition_ship',
     'ascent_ship',
+    'ice_metal_ship',
+    'lost_world_ship',
     'mario_ship'
 ]
