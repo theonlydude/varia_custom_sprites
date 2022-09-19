fa7a4ba181b61bad97c5d2551af18fa3ec169baf
theonlydude <theonlydude@server.fake>
2022-09-19 14:35:47 +0200

    custo: move sprites and ships images in dedicated dir

--- install.sh
+++ install.sh
@@ -15,8 +15,11 @@ CUR=$(pwd)/varia_custom_sprites
 CUSTOM_SPRITES=$(python3 -c "from varia_custom_sprites.custom_sprites import customSprites; print(' '.join(list(customSprites.keys())))")
 CUSTOM_SHIPS=$(python3 -c "from varia_custom_sprites.custom_ships import customShips; print(' '.join(list(customShips.keys())))")
 
-THUMBNAILS_DIR=~/web2py/applications/solver/static/images
+SPRITE_DIR=~/web2py/applications/solver/static/images/sprites
+SHIP_DIR=~/web2py/applications/solver/static/images/ships
 SHEETS_DIR=~/web2py/applications/solver/static/images/sprite_sheets
+
+mkdir -p ${SPRITE_DIR} ${SHIP_DIR} ${SHEETS_DIR}
 
 if [ ${CLEAN} -eq 0 ]; then
     echo "Clean existing web2py symlinks"
@@ -32,12 +35,12 @@ fi
 # add ln for thumbnails & sprite sheets in web2py
 echo "Create web2py symlinks"
 for SPRITE in ${CUSTOM_SPRITES}; do
-    [ -L ${THUMBNAILS_DIR}/${SPRITE}.png ] || ln -s ${CUR}/samus_sprites/${SPRITE}.png ${THUMBNAILS_DIR}/${SPRITE}.png
+    [ -L ${SPRITE_DIR}/${SPRITE}.png ] || ln -s ${CUR}/samus_sprites/${SPRITE}.png ${SPRITE_DIR}/${SPRITE}.png
     [ -L ${SHEETS_DIR}/${SPRITE}.png ] || ln -s ${CUR}/sprite_sheets/${SPRITE}.png ${SHEETS_DIR}/${SPRITE}.png
 done
 
 for SHIP in ${CUSTOM_SHIPS}; do
-    [ -L ${THUMBNAILS_DIR}/${SHIP}.png ] || ln -s ${CUR}/ship_sprites/${SHIP}.png ${THUMBNAILS_DIR}/${SHIP}.png
+    [ -L ${SHIP_DIR}/${SHIP}.png ] || ln -s ${CUR}/ship_sprites/${SHIP}.png ${SHIP_DIR}/${SHIP}.png
 done
 
 echo "done"
